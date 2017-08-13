91235768ea43e4c5c1c53ab49e8b95371eac6067
Audy Huang <od710122@gmail.com>
2017-08-13 18:31:39 +0800

    Show album cover in index.html.

--- flickr/views.py
+++ flickr/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from . import flickr_api
 
 # Create your views here.
 
+my_user_id = '45566434@N04'
+
 def index(request):
-    return render(request, 'flickr/index.html', {})
+    flickr = flickr_api.FlickrUtils()
+    response = flickr('get', 'photoset', 'getList',
+                        user_id=my_user_id, page='1', per_page='10',
+                        primary_photo_extras='url_s')
+    photoset_list = flickr.parse_photoset_list_response(response)
+    return render(request, 'flickr/index.html', {'photoset_list': photoset_list})
